6f845efd7818e5ecfc99a4294beae8841637eb92
Sergmakk <makarov_sergee@mail.ru>
2022-01-26 23:36:21 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey M");
+        System.out.println("18.04.1987");
+
+    }
+}
